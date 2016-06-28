78b41851de611d54a731317e10fd152253178330
Nimit Agg <nimitagg95@gmail.com>
2016-06-28 13:36:27 +0530

    main screen

--- app/src/main/java/com/example/helpmeson/MainActivity.java
+++ app/src/main/java/com/example/helpmeson/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.helpmeson;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.SharedPreferences;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences sp=getSharedPreferences("Pref",MODE_PRIVATE);
+        sp.contains("first");
     }
 }
