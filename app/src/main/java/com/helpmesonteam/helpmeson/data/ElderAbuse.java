5bb7603e8cf64d58ed559765f5ff14a71946b911
Hp <architgupta69@yahoo.in>
2016-07-03 15:53:01 +0530

    data added

---
+++ app/src/main/java/com/helpmesonteam/helpmeson/data/ElderAbuse.java
@@ -0,0 +1,92 @@
+package com.helpmesonteam.helpmeson.data;
+
+/**
+ * Created by Hp on 03-07-2016.
+ */
+public class ElderAbuse {
+    public String[] elder= new String[1];
+    void ElderAbuse(){
+        elder[0]="This is a trauma undergone by thousands of elders across India. Problem is, no one talks about it, no one reports it, and naturally no one stops it. Let us understand it to try and prevent it.\n" +
+                "\n" +
+                "Victims of Elder Abuse are often:\n" +
+                "Older than others, above 70 or 75 years\n" +
+                "Women are more likely to be victims\n" +
+                "Widows or living alone\n" +
+                "Socially isolated\n" +
+                "Having physical or cognitive problems\n" +
+                "Extreme dependence on care givers\n" +
+                "Abusers are often:\n" +
+                "Often younger than the person abused\n" +
+                "Close relative or care giver\n" +
+                "Living with the elder\n" +
+                "Psychologically stressed\n" +
+                "Financially dependent\n" +
+                "May have history of alcohol and drug use\n" +
+                "HOW TO SPOT IT\n" +
+                "It is not always easy to identify abuse. Many symptoms that are listed below, may result from the disease a person has or by the medication that is being prescribed. But there will be signs that cannot be explained away by medical reasons.\n" +
+                "Signs of Physical Abuse\n" +
+                "Bruises or grip marks around the arms or neck, rope marks or welts on the wrists and/or ankles\n" +
+                "Repeated unexplained injuries, dismissive attitude or statements about injuries\n" +
+                "Refusal to go to the same doctor or emergency service for repeated injuries\n" +
+                "There may be indications of burns, acute signs of hair and tooth loss, broken bones, or internal injuries\n" +
+                "Bruises, both old and new, particularly those in the shape of specific objects or even fingers, on areas such as the wrists, upper arms, inner thighs, and neck could be worrisome\n" +
+                "The improper use of a medication, such as an overdose of a tranquilizer, as a form of restraint\n" +
+                "Signs of Verbal / Emotional Abuse\n" +
+                "Hesitation in speaking openly with others, especially when a particular person is around\n" +
+                "Withdrawal and unresponsive to communication\n" +
+                "Fear and agitation\n" +
+                "Anxiety and helplessness\n" +
+                "Changes in sleep pattern, appetite\n" +
+                "Behavior unreasonably fearful or suspicious\n" +
+                "Lack of interest in social contacts\n" +
+                "Signs of Sexual Abuse\n" +
+                "Unexplained vaginal or anal bleeding\n" +
+                "Torn or bloody underwear\n" +
+                "Bruised breasts or buttocks\n" +
+                "Venereal diseases or vaginal infections\n" +
+                "Signs of Financial Abuse or Exploitation\n" +
+                "Lifestyle doesnt match the individuals financial assets\n" +
+                "Large withdrawals from bank accounts, accounts that have been switched\n" +
+                "Unusual ATM activity.\n" +
+                "Abrupt changes in a will, power of attorney, or financial documents\n" +
+                "Signatures on checks dont match the older persons signature\n" +
+                "Unexplained and sudden transfers of money/ property to a care giver\n" +
+                "Non-payment of bills which is not characteristic of the person.\n" +
+                "Unexplained disappearance of valuables and money\n" +
+                "Signs of Caregiver Neglect\n" +
+                "Lack of basic hygiene, poor personal hygiene\n" +
+                "Inadequate food and water, or clean and appropriate clothing\n" +
+                "Person with dementia left unsupervised\n" +
+                "Untreated pressure bedsores,\n" +
+                "Lack of medical aids (glasses, walker, teeth, hearing aid, medications)\n" +
+                "malnourishment or dehydration\n" +
+                "Unsafe or hazardous living conditions (lack of heat or running water, improper wiring).\n" +
+                "Signs of Self Neglect\n" +
+                "Indications of self-neglect are similar to those of neglect by a caregiver. Individuals may be unkempt, have poor dental and personal hygiene, or appear malnourished and/or dehydrated. They may not be taking medications properly or might appear listless, confused, or depressed. Their living environment may be dirty, lacking in electricity or water, and unsafe. Frequently there is a need for an assessment to determine the competency of the individual and his/her ability to appropriately manage health and safety issues.\n" +
+                "It is more common among very old people- age 85 and older. They may be confused and socially isolated. Family may be involved to a limited extent. The older person wants to remain independent. This poses several risks.\n" +
+                "HOW TO REPORT IT\n" +
+                "Ideally the problem of Elder Abuse has to be approached holistically. A variety of health and social services is required to identify the underlying causes of abuse, stop the abuse, reduce the likelihood that it will recur, and treat the abused for the negative impact of abuse.\n" +
+                "Mental Health Services\n" +
+                "The older person needs to be assessed to determine if he or she is capable of meeting the basic needs and make decisions and protect oneself. Assessment of abuser to determine their need for treatment and if they pose a danger to others is also required.\n" +
+                "Counseling\n" +
+                "Vulnerable adults or victims can be counseled to assess their options for safety and to resolve conflicts in family. Counseling will also help the abused overcome the trauma. Group or individual counseling may be used. Main objectives of such intervention would be: to educate victims about resources and options; break through denial and shame; make plans to avoid future abuse (safety planning); build support networks; and manage post-traumatic stress. Family counseling is useful to resolve or mediate conflicts and tensions in the family that give rise to neglect or abuse.\n" +
+                "Legal Assistance\n" +
+                "Subsidized or free legal aid should be available for victims. Legal help may be needed to recover property or assets, to restrain the abuser or restrict contact between perpetrators and victims, for guardianship issues (when the elder is not capable of taking care of self) and for prosecuting the offenders.\n" +
+                "Respite care and Support services\n" +
+                "Respite care programs at home, adult care and at institutions is vital to support families and carers. Caregiver stress may be reduced by providing services in the community that reduce burden and provide relief. Managing the finance of elders to avoid frauds, support groups for care givers, respite programs, home delivered meal programs for elders living alone, adult day-care services and friendly visitors to homes of isolated seniors, routine telephone ‘check-in’ programs to reassure lonely elders, are other support services.\n" +
+                "Violence Prevention programs\n" +
+                "Domestic violence programs may be extended to cover seniors also. Services such as crisis lines, shelters for abused, support and counseling services in safe homes would be required for abused elders also.\n" +
+                "Interventions for Abusers\n" +
+                "A range of physical, rehabilitative, behavioral and legal measures should be available to prevent abuser from repeating violent behavior.\n" +
+                "Sensitizing Public and Advocacy for Elders\n" +
+                "NGOs, voluntary groups, charitable trusts and Senior Citizens Associations can create awareness.\n" +
+                "HOW ELSE CAN YOU HELP\n" +
+                "There are various ways you can help prevent abuse in your neighborhood. All it takes is a little initiative and the will to help those who are vulnerable.\n" +
+                "Empowering elders in your area through awareness campaigns, informing them of the utility of the Maintenance and Welfare of Parents and Senior Citizens Act (2007)\n" +
+                "Provide elders with the local Police Station number which they can call in times of emergencies\n" +
+                "Give more power & provide Guidelines to local Police to take action against Elder Abuse\n" +
+                "Help in establishing Counseling Centers or use the existing Community centres for providing legal, financial, psycho – social help to elders.\n" +
+                "Form a Senior Citizens Forum in the community which can act as a powerful agency in cases of detection, prevention and amelioration of domestic abuse\n" +
+                "Set-up Vigilance Committees in urban residential colonies that need to work in collaboration with Residents Associations/Mohalla Committee of the area to deal with cases of Elder Abuse";
+    }
+}
---
+++ app/src/main/java/com/helpmesonteam/helpmeson/data/Wills.java
@@ -0,0 +1,62 @@
+package com.helpmesonteam.helpmeson.data;
+
+/**
+ * Created by Hp on 03-07-2016.
+ */
+public class Wills {
+    public  String[] will= new String[1];
+    void Wills(){
+        will[0]="What is a will?\n" +
+                "A will is a highly confidential document, the aim of which is to provide the person with a legal platform to distribute his/her assets after his/her demise.\n" +
+                "A will can be made on stamp paper, and it is advisable to get it registered at the office of the sub-registrar.\n" +
+                "If a person passes away without leaving behind a will, then his/her assets (both movable and immovable) will be divided among his/her legal heirs, as per the provisions mentioned in the Hindu Succession Act. Those who do not fall under the legal obligations of the Hindu Succession Act are governed by their personal law or by the Indian Succession Act.\n" +
+                "The distribution of assets, as mentioned in the will, cannot be challenged by anyone.\n" +
+                "However, it should be noted that a will is a revocable document, and can be changed by the ‘testator’ whenever he/she desires.\n" +
+                "In order to ensure that a will becomes a valid document, it has to be signed by at least 2 witnesses.\n" +
+                "It is advisable for all people, above the age of 45, to prepare a will.\n" +
+                "Advantages of making a will:\n" +
+                "The biggest advantage of preparing a will is that it makes the distribution of both movable and immovable property much easier.\n" +
+                "Through a will, it is also possible to reward a person/s (whether a relative or a non-relative), for his/her sincerity, loyalty and affection.\n" +
+                "The preparation of a will also provide the inheritor/s with huge tax planning advantages.\n" +
+                "Drafting a will\n" +
+                "The following steps should be followed while preparing a will-\n" +
+                "Make a list of all the movable and immovable assets owned.\n" +
+                "Category A consists of assets such as residential property, commercial property, machinery, antiques and other expensive items.\n" +
+                "Category B consists of cars, shares, securities and so on.\n" +
+                "Category C consists of items such as television sets, gadgets, and jewellery and so on.\n" +
+                "The next step is to make a list of all the prospective heirs.\n" +
+                "The 3rd step in the process is to compile a final list of all the heirs and the assets to be handed down to them.\n" +
+                "It must be noted that while preparing a will, the testator should also mention a list of the various liabilities that exist as on the date of the execution of the will. He/she must also prepare and mention a plan of action to deal with the issue of discharging these liabilities.\n" +
+                "A will also requires the appointment of an executor to execute the will.\n" +
+                "An important point to be remembered is that only self-acquired property can be willed away, not ancestral property.\n" +
+                "Registration of a will\n" +
+                "The registration of a will isnt necessary.\n" +
+                "If, however, the testator feels the need to register the will, he/she can do so at the office of the sub-registrar (at a nominal cost).\n" +
+                "For the purpose of registration of the will, in addition to the original copy, a duplicate copy has to be submitted too, which has to be signed by the person making the will.\n" +
+                "Two witnesses are required to authenticate the validity of the will.\n" +
+                "The testator should also provide documentary proof in regards to his/her identification (passport or voter ID card is sufficient).\n" +
+                "Once all the above mentioned steps have been followed, after 3 weeks or so, the original will, duly registered, will be returned.\n" +
+                "A will can also be registered with a notary public (generally appointed by the government).\n" +
+                "The role of the executor:\n" +
+                "An executor has the most vital role to play in the final execution of the will.\n" +
+                "It is the executor who is charged with the duty of executing the wishes and desires of the person who nominated him/her as the executor of the will.\n" +
+                "The major function of the executor is to collect all the assets and debts of the deceased person, pay off all the debts and finally, distribute the assets of the estate of the deceased person, according to the will.\n" +
+                "The executor of the will generally does not receive any form of remuneration for carrying out his/her duties.\n" +
+                "The executor of the will is advised to prepare a statement about the performance of his/her various duties in connection with the will.\n" +
+                "The role of a will in tax savings:\n" +
+                "The creation of a will provides various advantages that can be secured by the members of the family of the taxpayer.\n" +
+                "Transfer of assets without any consideration can be made by a will to one’s wife or daughter-in-law, without attracting the clubbing provisions of Section 64(I) of the IT Act.\n" +
+                "Another important tax advantage of a will is to secure an annual charge on property, particularly for the benefit of one’s daughters.\n" +
+                "One of the important means of tax planning to be adopted through the creation of a will is the creation of a Hindu Undivided Family.\n" +
+                "Under the provisions of Section 64(I), if a person bequeaths an asset to his minor child/grandchild, then the income accruing to the minor child/grandchild will be clubbed with the income of the mother/father. Hence, a will can be adopted as a proper instrument for the transfer of property by way of bequest, also aiding in tax-saving.\n" +
+                "Sections 64(I) (vi) and (viii) make provisions for the transfer in favour of the daughter-in-law, either directly or for her benefit. A bequest can be made in favour of the daughter-in-law, so as to confer an absolute title to her and make her a taxable entity (if she already isn’t one), after the testators demise.\n" +
+                "Through the creation of a will, a discretionary trust can be created, which would be taxed at normal rates. Such a trust is normally liable to be taxed at the maximum marginal rate of tax as per Section 164(I) of the Income Tax Act, 1961. However, certain types of exemptions to this provision are enlisted in the first provision of Section 164(I).\n" +
+                "A considerable amount of tax saving can also be achieved through the judicious drafting of a will.\n" +
+                "Nomination and will:\n" +
+                "General assumption is that a nominee for NSC, LIC polices, fixed deposits, Mutual funds, Provident Fund and Savings bank accounts are the legal heirs to the proceeds for which he or she was nominated.\n" +
+                "The nomination only indicates the person who is authorised to receive the amount on payment.\n" +
+                "The amount, however, can be claimed by the heirs of the assured in accordance with the law of succession governing them.\n" +
+                "Hence, one needs to have a WILL in place in order to ensure that the person of your choice receive the amount meaning thereby the person as nominee and receiver in the WILL are same person.\n" +
+                "Note: Please do not part with your original /photocopies of WILL and any other property documents in any circumstances because hostile person can destroy or make copies of same. The original documents should be kept in locker and only photo copies with you." ;
+    }
+}
